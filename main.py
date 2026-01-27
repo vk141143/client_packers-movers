ba6fab5cd34eb69b932f71b28cc51467dda92099
BindushreeGD <bindugd2001@gmail.com>
2026-01-27 15:45:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
